bed942c92393ef14ee8381a82d7cc308f913759e
wangqianping <1981517703@qq.com>
2020-05-04 04:45:12 +0800

    UDP

---
+++ src/socket/TestUDP.java
@@ -0,0 +1,34 @@
+package socket;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.*;
+
+public class TestUDP {
+
+
+    @Test
+    public void send() throws IOException {
+
+        DatagramSocket datagramSocket = new DatagramSocket();
+        String str = "我是通过UDP发送的";
+        DatagramPacket datagramPacket = new DatagramPacket(str.getBytes(),0,str.length(), InetAddress.getByName("127.0.0.1"),9090);
+        datagramSocket.send(datagramPacket);
+        datagramSocket.close();
+
+    }
+
+
+    @Test
+    public void receive() throws IOException {
+
+        DatagramSocket datagramSocket = new DatagramSocket(9090);
+        byte[] text = new byte[24];
+        DatagramPacket datagramPacket = new DatagramPacket(text,0,text.length);
+        datagramSocket.receive(datagramPacket);
+        System.out.println(new String(datagramPacket.getData(),0,datagramPacket.getData().length));
+        datagramSocket.close();
+
+    }
+}
